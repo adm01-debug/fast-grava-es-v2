e405c45e6ff65ee0066264dc4b044d2a56b608fa
adm01-debug <adm01@promobrindes.com.br>
2025-12-20 14:45:46 -0300

    feat: add VisuallyHidden accessibility component

---
+++ src/components/ui/VisuallyHidden.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface VisuallyHiddenProps {
+  children: React.ReactNode;
+  as?: keyof JSX.IntrinsicElements;
+}
+
+export function VisuallyHidden({ children, as: Component = 'span' }: VisuallyHiddenProps) {
+  return (
+    <Component
+      style={{
+        position: 'absolute',
+        width: '1px',
+        height: '1px',
+        padding: 0,
+        margin: '-1px',
+        overflow: 'hidden',
+        clip: 'rect(0, 0, 0, 0)',
+        whiteSpace: 'nowrap',
+        border: 0,
+      }}
+    >
+      {children}
+    </Component>
+  );
+}
+
+export default VisuallyHidden;
