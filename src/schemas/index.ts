13c7dcedefeb202d5c8a2aab5e3080c49dc58017
adm01-debug <adm01@promobrindes.com.br>
2025-12-20 14:45:24 -0300

    feat: add schemas index.ts

---
+++ src/schemas/index.ts
@@ -0,0 +1,6 @@
+// Schemas Index - Export all Zod schemas
+export * from './alertSchema';
+export * from './qualitySchema';
+export * from './reportSchema';
+export * from './energySchema';
+export * from './gamificationSchema';
