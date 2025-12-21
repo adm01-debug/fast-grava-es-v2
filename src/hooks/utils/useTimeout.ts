dc52f455cae86ce9ed81d6d5a07243223144e6bb
adm01-debug <adm01@promobrindes.com.br>
2025-12-21 08:53:57 -0300

    feat: add util hook useTimeout.ts

---
+++ src/hooks/utils/useTimeout.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef, useCallback } from 'react';
+
+export function useTimeout(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback);
+  const timeoutRef = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  const clear = useCallback(() => {
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+  }, []);
+
+  useEffect(() => {
+    if (delay === null) return;
+    timeoutRef.current = setTimeout(() => savedCallback.current(), delay);
+    return clear;
+  }, [delay, clear]);
+
+  return clear;
+}
