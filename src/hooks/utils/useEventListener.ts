2fc29b63151039b1eccd75c2af9789da42c12718
adm01-debug <adm01@promobrindes.com.br>
2025-12-21 08:53:45 -0300

    feat: add util hook useEventListener.ts

---
+++ src/hooks/utils/useEventListener.ts
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from 'react';
+
+export function useEventListener<K extends keyof WindowEventMap>(
+  eventName: K,
+  handler: (event: WindowEventMap[K]) => void,
+  element: Window | HTMLElement | null = window
+) {
+  const savedHandler = useRef(handler);
+
+  useEffect(() => { savedHandler.current = handler; }, [handler]);
+
+  useEffect(() => {
+    if (!element) return;
+    const eventListener = (event: Event) => savedHandler.current(event as WindowEventMap[K]);
+    element.addEventListener(eventName, eventListener);
+    return () => element.removeEventListener(eventName, eventListener);
+  }, [eventName, element]);
+}
