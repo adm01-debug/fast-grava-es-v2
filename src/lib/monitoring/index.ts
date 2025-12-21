362dbe9844851f378be23ddf5a78be5bed376ae3
adm01-debug <adm01@promobrindes.com.br>
2025-12-21 08:54:51 -0300

    feat: add monitoring index.ts

---
+++ src/lib/monitoring/index.ts
@@ -0,0 +1,17 @@
+// Monitoring exports
+export * from './sentry';
+export * from './analytics';
+export * from './webVitals';
+export * from './performance';
+export * from './logger';
+
+import { initSentry } from './sentry';
+import { initAnalytics } from './analytics';
+import { initWebVitals } from './webVitals';
+
+export function initMonitoring() {
+  initSentry();
+  initAnalytics();
+  initWebVitals();
+  console.log('[Monitoring] All monitoring services initialized');
+}
