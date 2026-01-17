d19d3ad4fac5d8a2244a0fc7fbb0bd57b682d78d
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 16:57:03 -0300

    chore: remove test file

--- e2e/operator-productivity.spec.ts
+++
@@ -1,4 +0,0 @@
-import { test, expect } from '@playwright/test';
-test.describe('Operator Productivity', () => {
-  test('loads page', async ({ page }) => { await page.goto('/operator-productivity'); await expect(page.locator('main')).toBeVisible(); });
-});
