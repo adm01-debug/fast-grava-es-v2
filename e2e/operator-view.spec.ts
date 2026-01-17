c6367ba7b2ea0d16fc32406eb1930067ef1c5635
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 16:57:03 -0300

    chore: remove test file

--- e2e/operator-view.spec.ts
+++
@@ -1,4 +0,0 @@
-import { test, expect } from '@playwright/test';
-test.describe('Operator View', () => {
-  test('loads page', async ({ page }) => { await page.goto('/operator'); await expect(page.locator('main')).toBeVisible(); });
-});
