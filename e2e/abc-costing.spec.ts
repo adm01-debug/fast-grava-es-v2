92b46103c4f1866e81fe9ab4f0b1f7194811e2b1
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 16:56:35 -0300

    chore: remove test file e2e/abc-costing.spec.ts

--- e2e/abc-costing.spec.ts
+++
@@ -1,4 +0,0 @@
-import { test, expect } from '@playwright/test';
-test.describe('ABC Costing', () => {
-  test('loads page', async ({ page }) => { await page.goto('/abc-costing'); await expect(page.locator('main')).toBeVisible(); });
-});
