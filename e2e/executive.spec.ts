709d7bc6dfe0452ecc52211d52903a539058993d
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 16:56:49 -0300

    chore: remove test file

--- e2e/executive.spec.ts
+++
@@ -1,4 +0,0 @@
-import { test, expect } from '@playwright/test';
-test.describe('Executive', () => {
-  test('loads page', async ({ page }) => { await page.goto('/executive'); await expect(page.locator('main')).toBeVisible(); });
-});
