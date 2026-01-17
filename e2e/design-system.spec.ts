39535e3c36b110c930818a7eb23903ad70a10a5b
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 16:56:41 -0300

    chore: remove test file e2e/design-system.spec.ts

--- e2e/design-system.spec.ts
+++
@@ -1,4 +0,0 @@
-import { test, expect } from '@playwright/test';
-test.describe('Design System', () => {
-  test('loads page', async ({ page }) => { await page.goto('/design-system'); await expect(page.locator('main')).toBeVisible(); });
-});
