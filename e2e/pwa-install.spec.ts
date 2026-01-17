54820cad076566432d014d0d3a4216348a5733ac
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 16:57:05 -0300

    chore: remove test file

--- e2e/pwa-install.spec.ts
+++
@@ -1,4 +0,0 @@
-import { test, expect } from '@playwright/test';
-test.describe('PWA Install', () => {
-  test('loads page', async ({ page }) => { await page.goto('/install'); await expect(page.locator('main')).toBeVisible(); });
-});
