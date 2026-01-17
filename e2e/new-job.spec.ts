14eee59062e6afad89ddf16050937db35960bee2
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 16:57:00 -0300

    chore: remove test file

--- e2e/new-job.spec.ts
+++
@@ -1,4 +0,0 @@
-import { test, expect } from '@playwright/test';
-test.describe('New Job', () => {
-  test('loads page', async ({ page }) => { await page.goto('/jobs/new'); await expect(page.locator('main')).toBeVisible(); });
-});
