96b9193dc42be15372ad99061496fdfbc945e11c
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 16:57:14 -0300

    chore: remove test file

--- e2e/spc.spec.ts
+++
@@ -1,4 +0,0 @@
-import { test, expect } from '@playwright/test';
-test.describe('SPC', () => {
-  test('loads page', async ({ page }) => { await page.goto('/spc'); await expect(page.locator('main')).toBeVisible(); });
-});
