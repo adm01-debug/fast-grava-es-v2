5cee35ebe24ca9f555cbeafc84549ead2809f7ed
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 16:56:48 -0300

    chore: remove test file

--- e2e/efficiency.spec.ts
+++
@@ -1,4 +0,0 @@
-import { test, expect } from '@playwright/test';
-test.describe('Efficiency', () => {
-  test('loads page', async ({ page }) => { await page.goto('/efficiency'); await expect(page.locator('main')).toBeVisible(); });
-});
