a2c3bc1a936516a08bcc706f4d410dddc5e53a7a
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 16:57:00 -0300

    chore: remove test file

--- e2e/ml-predictions.spec.ts
+++
@@ -1,4 +0,0 @@
-import { test, expect } from '@playwright/test';
-test.describe('ML Predictions', () => {
-  test('loads page', async ({ page }) => { await page.goto('/ml-predictions'); await expect(page.locator('main')).toBeVisible(); });
-});
