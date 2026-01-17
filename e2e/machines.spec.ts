fece660bfa02bfe9e4e8c58188f2ecc61c43388d
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 16:56:53 -0300

    chore: remove test file

--- e2e/machines.spec.ts
+++
@@ -1,4 +0,0 @@
-import { test, expect } from '@playwright/test';
-test.describe('Machines', () => {
-  test('loads page', async ({ page }) => { await page.goto('/machines'); await expect(page.locator('main')).toBeVisible(); });
-});
