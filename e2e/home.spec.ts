62904e59caab3681f8f23001d8c9fce5284ba19b
adm01-debug <adm01@promobrindes.com.br>
2026-01-17 16:56:51 -0300

    chore: remove test file

--- e2e/home.spec.ts
+++
@@ -1,4 +0,0 @@
-import { test, expect } from '@playwright/test';
-test.describe('Home', () => {
-  test('loads page', async ({ page }) => { await page.goto('/'); await expect(page.locator('main')).toBeVisible(); });
-});
